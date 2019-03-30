985fbbc3f1ba6728060eee85b87ccfb6c217a028
Paul Guszak <pguszak@yahoo.com>
2019-03-30 00:35:21 -0600

    first commit

---
+++ reviews.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+//got these three lines from the User.js
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB, { useNewUrlParser: true } );
+mongoose.set('useCreateIndex', true);
+
+var ReviewSchema = new Schema({
+    MovieTitle: {type: String, required: true},
+    Review: {type: String, required: true},
+    Stars: {type: Number, enum: ["1", "2", "3", "4", "5"], required: true}
+});
+
+var Review = mongoose.model("Review", ReviewSchema);
+module.exports = Review;
